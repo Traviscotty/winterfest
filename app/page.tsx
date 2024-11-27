97caaca0dfe950079965379f3caedb8cfe905550
Traviscotty <keklo6317@gmail.com>
2024-11-27 11:50:59 +0100

    neue bilder

--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,124 @@
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { boogie } from './fonts';
 
-export default function Home() {
-  return (
-    <div className="grid grid-cols-3 gap-1 h-full min-h-screen overflow-x-auto bg-black">
-      <div className="col-span-2 bg-blue-500">
-        01
-        <Image
-          src="/bmw.jpg" // Pfad zu deinem Bild
-          width={800}
-          height={400}
+export default function dashboard() {
+    return (
+
+<div>
+
+<div className="grid grid-cols-6 gap-0 h-full min-h-screen bg-zinc-300 ">
+  <div className="col-start-1 col-span-2 bg-white " 
+  style={{ backgroundImage: "url('/schneeflocken.jpg')" }}
+  >
+
+  <Image
+          src="/st.JPG" // Pfad zu deinem Bild
+          width={600}
+          height={200}
           alt="Bildbeschreibung 01"
-          className="pl-4"
+          className="pl-1 pt-1"
+          
         />
-      </div>
-      <div className="bg-white">
-        <h1 className="pl-40 underline italic font-bold">Informationen</h1>
-        <p className="pl-3  italic font-bold text-2xl"> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-      </div>
-      <div className="bg-white">03
-      
-      <Link href="/dashboard">
-          Link
-      </Link>
-      
-      </div>
-      <div className="col-span-2 bg-blue-500">
-        04
-        <Image
+    <h1 className='text-center text-4xl pt-1 underline text-bold italic'>Schuljahr 2023-2024</h1>
+</div>
+
+
+               {/* Container für die Überschrift zentrieren */}
+<div
+  className="col-start-3 col-end-7 bg-white "
+  style={{ backgroundImage: "url('/schneeflocken.jpg')" }}
+>
+  <h1 className={`${boogie.className} text-9xl text-sky-600 text-center  `}>
+    Winterfest
+  </h1>
+  {/* Button unter der Überschrift */}
+
+
+  <div className="text-center mt-8">
+  <Link href='/winterfest'>
+    <button
+      className="px-7 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 hover:scale-105 transition duration-300 focus:outline-none"
+    >
+      Mehr erfahren
+    </button>
+    </Link>
+  </div>
+</div>
+
+
+
+  <div className="col-start-1 col-end-4 bg-slate-100  relative"
+  >
+  <Image
           src="/bmw.jpg" // Pfad zu deinem Bild
-          width={800}
-          height={400}
           alt="Bildbeschreibung 01"
-          className="pl-4"
+          className="object-cover"
+          fill
+          sizes="100vw"
+          style={{
+            objectFit: 'cover', // cover, contain, none
+          }}
         />
-      </div>
-      <div className="col-span-2 bg-blue-500">
-        05
-        <Image
-          src="/bmw.jpg" // Pfad zu deinem Bild
-          width={800}
-          height={400}
-          alt="Bildbeschreibung 01"
-          className="pl-4"
-        />
-      </div>
-      <div className="bg-white">06</div>
-      <div className=" bg-white ">07</div>
-      <div className="col-span-2 bg-blue-500">
-        08{' '}
-        <Image
-          src="/bmw.jpg" // Pfad zu deinem Bild
-          width={800}
-          height={400}
-          alt="Bildbeschreibung 01"
-          className="pl-4"
-        />
-      </div>
-      <div className="col-span-2 bg-blue-500 ">
-        09
-        <Image
-          src="/bmw.jpg" // Pfad zu deinem Bild
-          width={800}
-          height={400}
-          alt="Bildbeschreibung 01"
-          className="pl-4"
-        />
-      </div>
-      <div className=" bg-white">10</div>
-      <div className=" bg-white ">11</div>
-      <div className="col-span-2 bg-blue-500 ">
-        12
-        <Image
-          src="/bmw.jpg" // Pfad zu deinem Bild
-          width={800}
-          height={400}
-          alt="Bildbeschreibung 01"
-          className="pl-4"
-        />
-      </div>
     </div>
-  );
+  <div className="col-end-7 col-span-3 bg-slate-100  relative"
+  >
+      <Image
+          src="/pferd.jpg" // Pfad zu deinem Bild
+          alt="Bildbeschreibung 01"
+          className=" object-cover"
+          fill
+          sizes="100vw"
+          style={{
+            objectFit: 'cover', // cover, contain, none
+          }}
+        />    </div>
+
+</div>
+
+
+
+</div>
+
+
+        );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/winterfest/page.tsx
@@ -0,0 +1,85 @@
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-3 gap-1 h-full min-h-screen overflow-x-auto bg-black">
+      <div className="col-span-2 bg-blue-500">
+        01
+        <Image
+          src="/bmw.jpg" // Pfad zu deinem Bild
+          width={800}
+          height={400}
+          alt="Bildbeschreibung 01"
+          className="pl-4"
+        />
+      </div>
+      <div className="bg-white">
+        <h1 className="pl-40 underline italic font-bold">Informationen</h1>
+        <p className="pl-3  italic font-bold text-2xl"> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="bg-white">03
+      
+      <Link href="/dashboard">
+          Link
+      </Link>
+      
+      </div>
+      <div className="col-span-2 bg-blue-500">
+        04
+        <Image
+          src="/bmw.jpg" // Pfad zu deinem Bild
+          width={800}
+          height={400}
+          alt="Bildbeschreibung 01"
+          className="pl-4"
+        />
+      </div>
+      <div className="col-span-2 bg-blue-500">
+        05
+        <Image
+          src="/bmw.jpg" // Pfad zu deinem Bild
+          width={800}
+          height={400}
+          alt="Bildbeschreibung 01"
+          className="pl-4"
+        />
+      </div>
+      <div className="bg-white">06</div>
+      <div className=" bg-white ">07</div>
+      <div className="col-span-2 bg-blue-500">
+        08{' '}
+        <Image
+          src="/bmw.jpg" // Pfad zu deinem Bild
+          width={800}
+          height={400}
+          alt="Bildbeschreibung 01"
+          className="pl-4"
+        />
+      </div>
+      <div className="col-span-2 bg-blue-500 ">
+        09
+        <Image
+          src="/bmw.jpg" // Pfad zu deinem Bild
+          width={800}
+          height={400}
+          alt="Bildbeschreibung 01"
+          className="pl-4"
+        />
+      </div>
+      <div className=" bg-white">10</div>
+      <div className=" bg-white ">11</div>
+      <div className="col-span-2 bg-blue-500 ">
+        12
+        <Image
+          src="/bmw.jpg" // Pfad zu deinem Bild
+          width={800}
+          height={400}
+          alt="Bildbeschreibung 01"
+          className="pl-4"
+        />
+      </div>
+    </div>
+  );
+}
